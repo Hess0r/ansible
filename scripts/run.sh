8fa7a881cfec03b1148083e0ee1b5618fa72b173
Sinka Gábor <gabor.sinka@mmsone.com>
2022-04-12 14:00:17 +0200

    Refactor scripts, and added ssh role

---
+++ scripts/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+PLAYBOOK_DIR="$HOME/Repos/ansible"
+CONFIG_DIR="$HOME/.config/dotfiles"
+
+if ! [[ -x "$(command -v ansible)" ]]; then
+  sudo add-apt-repository -y --update ppa:ansible/ansible
+  sudo apt install ansible
+fi
+
+if ! [[ -d "$PLAYBOOK_DIR" ]]; then
+  git clone "https://github.com/Hess0r/ansible.git" $PLAYBOOK_DIR
+else
+  git -C "$PLAYBOOK_DIR" pull
+fi
+
+ansible-galaxy install -r "$PLAYBOOK_DIR/requirements.yml"
+
+if [[ -f "$CONFIG_DIR/vault-password.txt" ]]; then
+  ansible-playbook --diff --extra-vars "@$CONFIG_DIR/values.yml" --vault-password-file "$CONFIG_DIR/vault-password.txt" "$PLAYBOOK_DIR/main.yml"
+else
+  ansible-playbook --diff --extra-vars "@$CONFIG_DIR/values.yml" "$PLAYBOOK_DIR/main.yml"
+fi
