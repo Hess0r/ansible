ae8033318521d4dc0f1ea7a786e8df57ce113401
Gabor Sinka <hessor@gmail.com>
2022-04-30 14:35:58 +0200

    Fix keymapping

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -274,7 +274,7 @@ cmp.setup({
     }),
     ['<Tab>'] = cmp.mapping.confirm({ select = true }),
     ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-    ['<C-p>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
