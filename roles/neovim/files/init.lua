1f8fd60e08085241b03715df92bfe65eef58b78a
Gabor Sinka <hessor@gmail.com>
2022-09-19 22:05:17 +0200

    Neovim todo

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -408,3 +408,5 @@ vim.api.nvim_create_user_command('T', 'vsp | terminal <args>', { nargs = "*" })
 
 --- PHP-CS-FIXER
 vim.g.php_cs_fixer_rules = "@PSR12"
+
+-- we need a way to set 'syntax enable'
